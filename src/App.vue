571b5848ca3c31161dc271230ad380c6d0a63c3c
Waqar Shaikh <waqarshaikh789@gmail.com>
2023-03-15 12:30:35 +0530

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BoardA />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoardA from "./components/Board.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BoardA,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgba(34, 148, 218, 0.454);
 }
 </style>
